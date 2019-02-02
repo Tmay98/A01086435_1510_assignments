e8599bba2a6429cb38d72776740049788a4a2249
Tmay98 <twmay98@hotmail.com>
2019-02-02 10:01:14 -0800

    added unit tests

--- A1/test_compound_interest.py
+++ A1/test_compound_interest.py
@@ -5,3 +5,6 @@ import compound_interest
 class TestCompound_interest(TestCase):
     def test_compound_interest(self):
         self.assertEqual(268.5063838389963, compound_interest.compound_interest(100, 0.10, 4, 10))
+
+    def test_no_interest(self):
+        self.assertEqual(100, compound_interest.compound_interest(100,0,2,4))
