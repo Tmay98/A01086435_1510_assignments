0cb1270b8fc731d3130dd4553947072794775b74
Tmay98 <twmay98@hotmail.com>
2019-02-02 09:59:53 -0800

    added unit tests

--- A1/test_convert_to_roman_numeral.py
+++ A1/test_convert_to_roman_numeral.py
@@ -5,4 +5,10 @@ import roman_numbers
 class TestConvert_to_roman_numeral(TestCase):
 
     def test_convert_to_roman_numeral(self):
-        self.assertEqual('V', roman_numbers.convert_to_roman_numeral(5))
+        self.assertEqual('CMXLIV', roman_numbers.convert_to_roman_numeral(944))
+
+    def test_lower_bound(self):
+        self.assertEqual('', roman_numbers.convert_to_roman_numeral(0))
+
+    def test_upper_bound(self):
+        self.assertEqual('MMMMMMMMMM', roman_numbers.convert_to_roman_numeral(10000))
