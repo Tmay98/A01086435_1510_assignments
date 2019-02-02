d269a132f55c979fc491171efb3da9a0f47a078f
Tmay98 <twmay98@hotmail.com>
2019-02-02 09:33:15 -0800

    initial commit

---
+++ A1/test_time_calculator.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+import time_calculator
+
+
+class TestTime_calculator(TestCase):
+    def test_time_calculator(self):
+        self.assertEqual(['days', 1, 'hours', 1, 'minutes', 42, 'seconds', 46], time_calculator.time_calculator(92566))
+
+
