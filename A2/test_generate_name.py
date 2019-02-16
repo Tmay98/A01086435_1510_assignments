d81b91d6edb04acbacaf36b58234cb18cc376ecf
Tommy <twmay98@hotmail.com>
2019-02-16 12:59:38 -0800

    more generate_names tests

--- A2/test_generate_name.py
+++ A2/test_generate_name.py
@@ -5,5 +5,21 @@ from dungeonsanddragons import generate_name
 class TestGenerate_name(TestCase):
 
     def test_correct_name_length(self):
-        name = generate_name(5)
-        self.assertTrue(len(name) == 10)
+        for i in range(20):
+            name = generate_name(i)
+            self.assertTrue(len(name) == (i*2))
+
+    def test_consonant_and_verb_order(self):
+        name = generate_name(20)
+        verbs = 'aeiouy'
+        consonants = 'bcdfghjklmnpqrstvwxz'
+        for i in range(len(name)):
+            if i % 2 == 1:
+                self.assertIn(name[i], verbs)
+            else:
+                self.assertIn(name[i], consonants)
+
+    def test_wrong_input(self):
+        with self.assertRaises(TypeError):
+            generate_name('ferg')
+
