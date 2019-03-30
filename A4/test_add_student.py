1d301beb5185b44ff1cd97231a0c7582e712493a
Tommy <twmay98@hotmail.com>
2019-03-29 18:17:13 -0700

    fixed unit tests

--- A4/test_add_student.py
+++ A4/test_add_student.py
@@ -1,44 +1,49 @@
 from unittest import TestCase
-from Student import add_student
-from Student import  Student
+from Student_Record import add_student
+from Student import Student
 from unittest.mock import patch
 import io
 
 
 class TestAdd_student(TestCase):
 
+    @patch('builtins.input', side_effect=['tommy may A01086435'])
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_add_student_not_enough_input(self, mock_stdout):
-        add_student('tommy may A01086435')
+    def test_add_student_not_enough_input(self, mock_stdout, mock_input):
+        add_student()
         actual_output = mock_stdout.getvalue()
         expected_output = 'You did not enter all required fields for the student' \
                           ' (FirstName, LastName, Student#, standing)\n\n'
         self.assertEqual(expected_output, actual_output)
 
+    @patch('builtins.input', side_effect=['34fl; may A01086435 True'])
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_add_student_incorrect_first_name(self, mock_stdout):
-        add_student('34fl; may A01086435 True')
+    def test_add_student_incorrect_first_name(self, mock_stdout, mock_input):
+        add_student()
         actual_output = mock_stdout.getvalue()
-        expected_output = 'You did not enter correct info for the new student\n'
+        expected_output = 'You entered an incorrect first name\n'
         self.assertEqual(expected_output, actual_output)
 
+    @patch('builtins.input', side_effect=['tommy ;3[2f A01086435 True'])
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_add_student_incorrect_last_name(self, mock_stdout):
-        add_student('tommy ;3[2f A01086435 True')
+    def test_add_student_incorrect_last_name(self, mock_stdout, mock_input):
+        add_student()
         actual_output = mock_stdout.getvalue()
-        expected_output = 'You did not enter correct info for the new student\n'
+        expected_output = 'You entered an incorrect last name\n'
         self.assertEqual(expected_output, actual_output)
 
+    @patch('builtins.input', side_effect=['tommy may A013gvf6435 True'])
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_add_student_incorrect_student_number(self, mock_stdout):
-        add_student('tommy may A013gvf6435 True')
+    def test_add_student_incorrect_student_number(self, mock_stdout, mock_input):
+        add_student()
         actual_output = mock_stdout.getvalue()
-        expected_output = 'You did not enter correct info for the new student\n'
+        expected_output = 'You entered an incorrect student number\n'
         self.assertEqual(expected_output, actual_output)
 
+    @patch('builtins.input', side_effect=['tommy may A01086435 True 50 30 40'])
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_add_student_with_grades(self, mock_stdout):
-        add_student('tommy may A01086435 True 50 30 40')
+    def test_add_student_with_grades(self, mock_stdout, mock_input):
+        add_student()
         actual_output = mock_stdout.getvalue()
         expected_output = 'student added to file\n'
         self.assertEqual(expected_output, actual_output)
