35765ce03dc17974e9c1fb5a9976c3e8ad429175
Tommy <twmay98@hotmail.com>
2019-03-30 10:34:19 -0700

    initial commit

---
+++ A4/test_convert_to_bool.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+from Student_Record import convert_to_bool
+
+
+class TestConvert_to_bool(TestCase):
+
+    def test_convert_to_bool_True(self):
+        self.assertEqual(convert_to_bool('True'), True)
+
+    def test_convert_to_bool_Not_Capitalized(self):
+        self.assertEqual(convert_to_bool('false'), False)
+
+    def test_convert_to_bool_incorrect_input(self):
+        with self.assertRaises(ValueError):
+            convert_to_bool('ksdo')
+
+
---
+++ A4/test_file_write.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+from Student_Record import file_write
+from Student import Student
+
+
+class TestFile_write(TestCase):
+
+    def setUp(self):
+        self.testStudent1 = Student('Tommy', 'May', 'A01086435', True, ['90'])
+
+    def test_file_write(self):
+        expected_output = '\nTommy May A01086435 True 90'
+        open('students.txt', 'w').close()
+        file_write(self.testStudent1)
+        with open('students.txt', 'r') as f_obj:
+            actual_output = f_obj.read()
+        self.assertEqual(expected_output, actual_output)
+
+    def test_file_write_appends(self):
+        expected_output = 'stuff\nTommy May A01086435 True 90'
+        with open('students.txt', 'w') as f_obj:
+            f_obj.write('stuff')
+        file_write(self.testStudent1)
+        with open('students.txt', 'r') as f_obj:
+            actual_output = f_obj.read()
+        self.assertEqual(expected_output, actual_output)
+
