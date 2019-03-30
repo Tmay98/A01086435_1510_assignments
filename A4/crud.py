d6a1f137ca9d41a0a507b01b7f4f5ba57ad9f8dc
Tommy <twmay98@hotmail.com>
2019-03-29 18:15:55 -0700

    took out input / print statements and moved them to functions

--- A4/crud.py
+++ A4/crud.py
@@ -3,7 +3,7 @@
 # Tommy May
 # A01086435
 
-import Student
+import Student_Record
 
 
 def main():
@@ -12,28 +12,19 @@ def main():
         selection = input("Select an option in the menu:\n1. Add student\n2. Delete student\n"
                           "3. Calculate class avg\n4. Print class list\n5. Edit student grades\n6: Quit\n")
         if selection == '1':
-            student_info = input('Enter the information for the new student in format:\n'
-                                 ' FirstName LastName Student# Standing Grades\n')
-            Student.add_student(student_info)
+            Student_Record.add_student()
         elif selection == '2':
-            delete_student = input('Enter the student number of the student to be deleted\n')
-            student_deleted = Student.file_delete_student(delete_student)
-            if student_deleted:
-                print('student deleted successfully')
-            else:
-                print('student not found')
+            Student_Record.file_delete_student()
         elif selection == '3':
-            Student.calculate_class_average()
+            Student_Record.calculate_class_average()
         elif selection == '4':
-            Student.print_class_list()
+            Student_Record.print_class_list()
         elif selection == '5':
-            student_number = input('Enter the student number of the student to add grades to\n')
-            Student.edit_student_grades(student_number)
+            Student_Record.edit_student_grades()
         elif selection == '6':
             continue
         else:
             print('incorrect option, please input a number between 1 and 5')
-
 
 
 if __name__ == "__main__":
