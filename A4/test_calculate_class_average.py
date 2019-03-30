818b858a19031f6aaf48ee417133302f59aabfe9
Tommy <twmay98@hotmail.com>
2019-03-30 14:44:35 -0700

    initial commit

---
+++ A4/test_calculate_class_average.py
@@ -0,0 +1,37 @@
+from unittest import TestCase
+from Student_Record import calculate_class_average
+from unittest.mock import patch
+import io
+
+
+class TestCalculate_class_average(TestCase):
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_calculate_class_average(self, mock_output):
+        with open('students.txt', 'w') as f_obj:
+            f_obj.write('person three A01086435 True 90 80 70\n')
+            f_obj.write('person two A01086436 True 100 60\n')
+        calculate_class_average()
+        actual_output = mock_output.getvalue()
+        expected_output = 'the class average is: 80.0 %\n\n'
+        self.assertEqual(actual_output, expected_output)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_student_with_no_grades_not_added_to_average(self, mock_output):
+        with open('students.txt', 'w') as f_obj:
+            f_obj.write('person three A01086435 True 90 80 70\n')
+            f_obj.write('person two A01086436 True 100 60\n')
+            f_obj.write('person three A01086437 True\n')
+        calculate_class_average()
+        actual_output = mock_output.getvalue()
+        expected_output = 'the class average is: 80.0 %\n\n'
+        self.assertEqual(actual_output, expected_output)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_empty_students_list(self, mock_output):
+        with open('students.txt', 'w') as f_obj:
+            pass
+        calculate_class_average()
+        actual_output = mock_output.getvalue()
+        expected_output = 'No students in file\n'
+        self.assertEqual(actual_output, expected_output)
