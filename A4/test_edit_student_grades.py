7c55b21b74cfbd834bdf8efd3cda03b2b259d238
Tommy <twmay98@hotmail.com>
2019-03-30 21:17:55 -0700

    initial commit

---
+++ A4/test_edit_student_grades.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+from Student_Management_System.Student_Updating import edit_student_grades
+import io
+from unittest.mock import patch
+
+
+class TestEdit_student_grades(TestCase):
+
+    @patch('builtins.input', side_effect=['A01086435', '90', 'done'])
+    def test_edit_student_grades(self, mock_input):
+        with open('students.txt', 'w') as f_obj:
+            f_obj.write('Tommy May A01086435 True 90')
+        self.assertTrue(edit_student_grades())
+
+    @patch('builtins.input', side_effect=['A01034rf34435'])
+    def test_invalid_student_number(self, mock_input):
+        with open('students.txt', 'w') as f_obj:
+            f_obj.write('Tommy May A01086435 True 90')
+        self.assertFalse(edit_student_grades())
+
+    @patch('builtins.input', side_effect=['A01086433'])
+    def test_student_not_in_file(self, mock_input):
+        with open('students.txt', 'w') as f_obj:
+            f_obj.write('Tommy May A01086435 True 90')
+        self.assertFalse(edit_student_grades())
