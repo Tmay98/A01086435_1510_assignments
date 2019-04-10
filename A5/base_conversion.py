be2e4773e7a0a53c5e9df94906879a21ce9b5dc3
Tommy <twmay98@hotmail.com>
2019-04-10 13:14:46 -0700

    converts to desired base

--- A5/base_conversion.py
+++ A5/base_conversion.py
@@ -5,16 +5,26 @@
 
 
 def base_conversion(base_from, number: int, base_to):
-    converted_number = 0
+    base_10_number = 0
     digits_list = [int(digit) for digit in str(number)]
+    converted_number_list = []
 
     # convert number to base 10
     for i in range(len(digits_list)):
-        converted_number += digits_list[len(digits_list) - 1 - i] * base_from ** i
+        base_10_number += digits_list[len(digits_list) - 1 - i] * base_from ** i
+
+    # converts number to desired base from base 10
+    while base_10_number > 0:
+        converted_number_list.append(base_10_number % base_to)
+        base_10_number = base_10_number // base_to
+    converted_number_list.reverse()
+    # converts list to string and makes it an integer
+    converted_number = int(''.join(str(i) for i in converted_number_list))
+    return converted_number
 
 
 def main():
-    base_conversion(5, 43, 10)
+    print(base_conversion(10, 256, 2))
 
 
 if __name__ == '__main__':
