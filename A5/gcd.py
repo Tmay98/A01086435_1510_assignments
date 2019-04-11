377087dc7d0773045719a6d9e88bcfe9b53225ac
Tommy <twmay98@hotmail.com>
2019-04-11 09:18:54 -0700

    doctest added

--- A5/gcd.py
+++ A5/gcd.py
@@ -1,21 +1,37 @@
-"""gcd calculator function"""
+"""gcd calculator function calculates the gcd of 2 integers"""
 
 # Tommy May
 # A01086435
 
 from math import floor
+import doctest
 
 
 def gcd(a: int, b: int):
+    """ Calculates the gcd of 2 integers
+
+    PARAM: a an int
+    PRECONDITION: a must be an int
+    PARAM: b an int
+    PRECONDITION: b must be an int
+    POSTCONDITION: gcd of a and b is calculated
+    RETURN: gcd of a and b
+
+    >>> gcd(25, 15)
+    5
+    >>> gcd(15, -25)
+    5
+    """
     if a == 0:
-        return b
+        return abs(b)
     elif b == 0:
-        return a
+        return abs(a)
     else:
         return gcd(b, a - b * floor(a / b))
 
 
 def main():
+    doctest.testmod()
     print(gcd(-25, 15))
 
 
