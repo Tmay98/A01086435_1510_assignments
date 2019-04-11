6d20cd22e4f0fc8f54b45d4a415137e7c97d4fae
Tommy <twmay98@hotmail.com>
2019-04-11 09:22:15 -0700

    doctest added

--- A5/selection_sort.py
+++ A5/selection_sort.py
@@ -1,10 +1,24 @@
-"""selection_sort function"""
+"""selection_sort function sorts a list of sortable items"""
 
 # Tommy May
 # A01086435
 
+import doctest
+
 
 def selection_sort(unsorted_list):
+    """Sorts a list of sortable items from lowest to highest
+
+    PARAM: unsorted_list a list
+    PRECONDITION: unsorted_list is a list of sortable items
+    POSTCONDITION: unsorted_list is sorted
+    RETURN: the sorted list
+
+    >>> selection_sort(['a', 'f', 'b', 'l', 'z', 'c'])
+    ['a', 'b', 'c', 'f', 'l', 'z']
+    >>> selection_sort([1, 5, 25, 3, 15, 2, 54, 32, 7])
+    [1, 2, 3, 5, 7, 15, 25, 32, 54]
+    """
     sorted_list = []
     length = len(unsorted_list)
     for i in range(length):
