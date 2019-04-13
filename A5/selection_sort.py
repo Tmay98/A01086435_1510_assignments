be9d793ec4a9386c900419234fd705da82951d66
Tommy <twmay98@hotmail.com>
2019-04-12 20:25:02 -0700

    raises errors for empty list and list of sequence elements

--- A5/selection_sort.py
+++ A5/selection_sort.py
@@ -4,6 +4,7 @@
 # A01086435
 
 import doctest
+from collections.abc import Sequence
 
 
 def selection_sort(unsorted_list):
@@ -19,6 +20,15 @@ def selection_sort(unsorted_list):
     >>> selection_sort([1, 5, 25, 3, 15, 2, 54, 32, 7])
     [1, 2, 3, 5, 7, 15, 25, 32, 54]
     """
+    # check for empty list
+    if not unsorted_list:
+        raise TypeError('empty list')
+
+    # check if list contains sequence elements not including strings
+    for i in range(len(unsorted_list)):
+        if isinstance(unsorted_list[i], Sequence) and not isinstance(unsorted_list[i], str):
+            raise TypeError('list elements not sortable')
+
     sorted_list = []
     length = len(unsorted_list)
     for i in range(length):
@@ -32,7 +42,24 @@ def selection_sort(unsorted_list):
 
 
 def main():
+    doctest.testmod()
     print(selection_sort(['c', 'a', 'd', 'g', 'b', 'l']))
+    print(selection_sort([1, 5, 2, 7, 15, 3]))
+    
+    try:
+        selection_sort([])
+    except TypeError as e:
+        print(e)
+
+    try:
+        selection_sort([[1, 2, 3], [2, 3, 4]])
+    except TypeError as e:
+        print(e)
+
+    try:
+        selection_sort([(1, 2, 3), (2, 3, 4)])
+    except TypeError as e:
+        print(e)
 
 
 if __name__ == '__main__':
