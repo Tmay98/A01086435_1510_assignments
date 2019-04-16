d9fb2377e06fc6a0de437b5ae300138d1bf02121
Tommy <twmay98@hotmail.com>
2019-04-15 23:13:31 -0700

    added another unit test

--- A5/test_website.py
+++ A5/test_website.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 from website import website
 from unittest.mock import patch
 import os
+import re
 
 
 class TestWebsite(TestCase):
@@ -15,4 +16,18 @@ class TestWebsite(TestCase):
         except FileNotFoundError:
                 self.fail('file wasnt created')
         os.remove('index.html')
+
+    @patch('builtins.input', side_effect=['tommy', 'info'])
+    def test_website_contents(self, mock_input):
+        website()
+        with open('index.html') as f_obj:
+            contents = f_obj.read()
+        name_regex = re.compile(r'tommy')
+        info_regex = re.compile(r'info')
+        match_object_1 = name_regex.search(contents)
+        match_object_2 = info_regex.search(contents)
+        if not match_object_1 and match_object_2:
+            self.fail()
+        os.remove('index.html')
+
 
